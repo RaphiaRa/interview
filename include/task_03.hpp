aea6ac0606be9dcc896e54ff6eb29f73e2b51709
RaphiaRa <raphia1992@gmail.com>
2021-11-16 09:51:09 +0100

    Typo

--- include/task_03.hpp
+++ include/task_03.hpp
@@ -1,7 +1,7 @@
 /** task_03.hpp
  * Task:
  * Finish the implementation of the classes Base, Apple & Banana such that
- * the function getMsg(const Base&) return 
+ * the function getMsg(const Base&) returns
  * "My name is Base" if a reference to a Base object is passed
  * "My name is Apple" if a reference to an Apple object is passed
  * "My name is Banana" if a reference to a Banana obhect is passed
@@ -30,7 +30,7 @@ namespace instar
         /** Todo **/  
     };
 
-    /** Don't change anything from this line **/
+    /** Don't change anything from this line on **/
     std::string getMsg(const Base& base) {return base.msg();}
 }
 #endif
